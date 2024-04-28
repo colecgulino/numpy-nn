f7ef78edf9da1d8e3005a62e475e5d3f84815753
Cole Gulino <colegulino@macbook-pro.lan>
2024-04-28 14:44:21 -0700

    Add transformer module.

---
+++ transformer.py
@@ -0,0 +1,124 @@
+"""Transformer network module."""
+
+import numpy as np
+
+import activations
+import attention
+import dense
+import layer
+import normalization
+
+
+class FFN(layer.Layer):
+    """Feed Forward Network layer for a transformer."""
+
+    def __init__(
+            self,
+            embed_dim: int,
+            use_bias: bool = True,
+            name: str = 'FFN'
+    ) -> None:
+        super().__init__(name)
+        self.embed_dim = embed_dim
+        self.use_bias = use_bias
+
+        self.ffn1 = dense.Dense(embed_dim, embed_dim * 4, name=f'{name}/Dense1')
+        self.ffn2 = dense.Dense(embed_dim * 4, embed_dim, name=f'{name}/Dense2')
+        self.act = activations.ReLU()
+    
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Implements a simple FFN (ffn1 -> act -> ffn2)."""
+        h1 = self.ffn1(x)
+        a1 = self.act(h1)
+        return self.ffn2(self.act(self.ffn1(x))), {'h1': h1, 'a1': a1}
+
+    def backward(
+            self,
+            x: np.ndarray,
+            cache: layer.Cache,
+            backwards_gradient: np.ndarray,
+            gradients: dict[str, np.ndarray]
+    ) -> np.ndarray:
+        """Backwards pass of the FFN network. See Dense for more details."""
+        h1, a1 = cache['h1'], cache['a1']
+        backwards_gradient = self.ffn2.backward(a1, {}, backwards_gradient, gradients)
+        backwards_gradient = self.act.backward(h1, {}, backwards_gradient, gradients)
+        return self.ffn1.backward(x, {}, backwards_gradient, gradients)
+
+
+class Block(layer.Layer):
+    """Single transformer block with prenorm."""
+
+    def __init__(
+            self,
+            embed_dim: int,
+            use_bias: bool = True,
+            name: str = 'Block'
+    ) -> None:
+        super().__init__(name)
+        self.attn = attention.MaskedSelfAttention(
+            embed_dim=embed_dim,
+            use_bias=use_bias,
+            name=f'{name}/Attn'
+        )
+        self.ln_attn = normalization.LayerNorm(
+            embed_dim, name=f'{name}/LNAttn'
+        )
+        self.ffn = FFN(
+            embed_dim=embed_dim,
+            use_bias=use_bias,
+            name=f'{name}/FFN'
+        )
+        self.ln_ffn = normalization.LayerNorm(
+            embed_dim, name=f'{name}/LNFFN'
+        )
+
+    def forward(self, x: np.ndarray) -> tuple[np.ndarray, layer.Cache]:
+        """Simple forward pass of the Transformer Block.
+        
+        Implements:
+            x = x + self.attn(self.ln_attn(x))
+            y = x + self.ffn(self.ln_ffn(x))
+        """
+        attn_in = self.ln_attn(x)
+        attn_out, attn_cache = self.attn.forward(attn_in)
+        ln_ffn_in = x + attn_out
+        ffn_in = self.ln_ffn(ln_ffn_in)
+        ffn_out, ffn_cache = self.ffn.forward(ffn_in)
+        out = ln_ffn_in + ffn_out
+        cache = {
+            'attn_in': attn_in,
+            'ln_ffn_in': ln_ffn_in,
+            'ffn_in': ffn_in,
+            'ffn_cache': ffn_cache,
+            'attn_cache': attn_cache
+        }
+        return out, cache
+
+    def backward(
+            self,
+            x: np.ndarray,
+            cache: layer.Cache,
+            backwards_gradient: np.ndarray,
+            gradients: dict[str, np.ndarray]
+    ) -> np.ndarray:
+        attn_in, ln_ffn_in, ffn_in, = (
+            cache['attn_in'], cache['ln_ffn_in'], cache['ffn_in']
+        )
+
+        ffn_backwards = self.ffn.backward(
+            ffn_in, cache['ffn_cache'], backwards_gradient, gradients
+        )
+        ln_ffn_backwards = self.ln_ffn.backward(
+            ln_ffn_in, {}, ffn_backwards, gradients
+        )
+        attn_backwards = self.attn.backward(
+            attn_in,
+            cache['attn_cache'],
+            ln_ffn_backwards + backwards_gradient,
+            gradients
+        )
+        ln_attn_backwards = self.ln_attn.backward(
+            x, {}, attn_backwards, gradients
+        )
+        return backwards_gradient + ln_attn_backwards + ln_ffn_backwards
---
+++ transformer_test.py
@@ -0,0 +1,186 @@
+import math
+import unittest
+
+import numpy as np
+from numpy import testing as np_test
+import torch
+from torch import nn
+
+import losses
+import optimizers
+import transformer
+
+
+NUM_BLOCKS = 5
+
+
+class MSATorch(torch.nn.Module):
+
+    def __init__(self, embed_dim: int) -> None:
+        super().__init__()
+        self.embed_dim = embed_dim
+
+        self.q_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+        self.k_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+        self.v_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+        self.output_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+    
+    def forward(self, x: torch.Tensor, mask: torch.Tensor | None = None) -> torch.Tensor:
+        B, T, C = x.shape
+        q = self.q_proj(x)
+        k = self.k_proj(x)
+        v = self.v_proj(x)
+
+        scale = 1. / math.sqrt(k.shape[-1])
+        attn = q @ k.transpose(-2, -1) * scale
+        if mask is not None:
+            attn = attn.masked_fill(mask, -float("inf"))
+        scores = torch.nn.functional.softmax(attn, dim=-1)
+        output = scores @ v
+        return self.output_proj(output)
+
+
+class PTBlock(nn.Module):
+
+    def __init__(self, embed_dim: int) -> None:
+        super().__init__()
+        self.attn = MSATorch(embed_dim)
+        self.ln_attn = nn.LayerNorm(embed_dim)
+        self.ffn1 = nn.Linear(embed_dim, embed_dim * 4)
+        self.ffn2 = nn.Linear(embed_dim * 4, embed_dim)
+        self.act = nn.ReLU()
+        self.ln_ffn = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        temp = x + self.attn(self.ln_attn(x))
+        return temp + self.ffn2(
+            self.act(self.ffn1(self.ln_ffn(temp)))
+        )
+
+
+class TestTransformer(unittest.TestCase):
+
+    def test_forward(self):
+        x = torch.ones((5, 10, 10))
+
+        pt_blocks = []
+        parameters = {}
+        for i in range(NUM_BLOCKS):
+            pt_blocks.append(PTBlock(10))
+            parameters.update({
+                f'Block{i}/LNFFN/scale': pt_blocks[-1].ln_ffn.weight,
+                f'Block{i}/LNFFN/shift': pt_blocks[-1].ln_ffn.bias,
+                f'Block{i}/FFN/Dense1/W': pt_blocks[-1].ffn1.weight.T,
+                f'Block{i}/FFN/Dense1/b': pt_blocks[-1].ffn1.bias,
+                f'Block{i}/FFN/Dense2/W': pt_blocks[-1].ffn2.weight.T,
+                f'Block{i}/FFN/Dense2/b': pt_blocks[-1].ffn2.bias,
+                f'Block{i}/Attn/DenseQ/W': pt_blocks[-1].attn.q_proj.weight.T,
+                f'Block{i}/Attn/DenseQ/b': pt_blocks[-1].attn.q_proj.bias,
+                f'Block{i}/Attn/DenseK/W': pt_blocks[-1].attn.k_proj.weight.T,
+                f'Block{i}/Attn/DenseK/b': pt_blocks[-1].attn.k_proj.bias,
+                f'Block{i}/Attn/DenseV/W': pt_blocks[-1].attn.v_proj.weight.T,
+                f'Block{i}/Attn/DenseV/b': pt_blocks[-1].attn.v_proj.bias,
+                f'Block{i}/Attn/DenseO/W': pt_blocks[-1].attn.output_proj.weight.T,
+                f'Block{i}/Attn/DenseO/b': pt_blocks[-1].attn.output_proj.bias,
+                f'Block{i}/LNAttn/scale': pt_blocks[-1].ln_attn.weight,
+                f'Block{i}/LNAttn/shift': pt_blocks[-1].ln_attn.bias,
+            })
+        parameters = {k: v.detach().numpy() for k, v in parameters.items()}
+
+        my_blocks = []
+        for i in range(NUM_BLOCKS):
+            my_blocks.append(transformer.Block(10, 1, name=f'Block{i}'))
+            my_blocks[-1].update_parameters(parameters)
+        
+        y = x.detach().numpy()
+        for block in my_blocks:
+            y = block(y)
+        y2 = nn.Sequential(*pt_blocks)(x).detach().numpy()
+        np_test.assert_allclose(y, y2, atol=1e-6, rtol=1e-6)
+
+    def test_backward(self):
+        x = torch.ones((5, 10, 10))
+
+        pt_blocks = []
+        parameters = {}
+        for i in range(NUM_BLOCKS):
+            pt_blocks.append(PTBlock(10))
+            parameters.update({
+                f'Block{i}/LNFFN/scale': pt_blocks[-1].ln_ffn.weight,
+                f'Block{i}/LNFFN/shift': pt_blocks[-1].ln_ffn.bias,
+                f'Block{i}/FFN/Dense1/W': pt_blocks[-1].ffn1.weight.T,
+                f'Block{i}/FFN/Dense1/b': pt_blocks[-1].ffn1.bias,
+                f'Block{i}/FFN/Dense2/W': pt_blocks[-1].ffn2.weight.T,
+                f'Block{i}/FFN/Dense2/b': pt_blocks[-1].ffn2.bias,
+                f'Block{i}/Attn/DenseQ/W': pt_blocks[-1].attn.q_proj.weight.T,
+                f'Block{i}/Attn/DenseQ/b': pt_blocks[-1].attn.q_proj.bias,
+                f'Block{i}/Attn/DenseK/W': pt_blocks[-1].attn.k_proj.weight.T,
+                f'Block{i}/Attn/DenseK/b': pt_blocks[-1].attn.k_proj.bias,
+                f'Block{i}/Attn/DenseV/W': pt_blocks[-1].attn.v_proj.weight.T,
+                f'Block{i}/Attn/DenseV/b': pt_blocks[-1].attn.v_proj.bias,
+                f'Block{i}/Attn/DenseO/W': pt_blocks[-1].attn.output_proj.weight.T,
+                f'Block{i}/Attn/DenseO/b': pt_blocks[-1].attn.output_proj.bias,
+                f'Block{i}/LNAttn/scale': pt_blocks[-1].ln_attn.weight,
+                f'Block{i}/LNAttn/shift': pt_blocks[-1].ln_attn.bias,
+            })
+        parameters = {k: v.detach().numpy() for k, v in parameters.items()}
+
+        my_blocks = []
+        for i in range(NUM_BLOCKS):
+            my_blocks.append(transformer.Block(10, 1, name=f'Block{i}'))
+            my_blocks[-1].update_parameters(parameters)
+
+        y = x.detach().numpy()
+        for block in my_blocks:
+            y = block(y)
+        y2 = nn.Sequential(*pt_blocks)(x)
+
+        y_hat = torch.tensor(
+            [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.]
+        )[None, None].repeat(5, 10, 1)
+        loss = nn.CrossEntropyLoss()(
+            y2.contiguous().view(-1, 10), y_hat.contiguous().view(-1, 10)
+        )
+        loss.backward()
+
+        reference_gradients = {}
+        for i in range(NUM_BLOCKS):
+            pt_blocks.append(PTBlock(10))
+            reference_gradients.update({
+                f'Block{i}/LNFFN/scale': pt_blocks[i].ln_ffn.weight.grad,
+                f'Block{i}/LNFFN/shift': pt_blocks[i].ln_ffn.bias.grad,
+                f'Block{i}/FFN/Dense1/W': pt_blocks[i].ffn1.weight.grad.T,
+                f'Block{i}/FFN/Dense1/b': pt_blocks[i].ffn1.bias.grad,
+                f'Block{i}/FFN/Dense2/W': pt_blocks[i].ffn2.weight.grad.T,
+                f'Block{i}/FFN/Dense2/b': pt_blocks[i].ffn2.bias.grad,
+                f'Block{i}/Attn/DenseQ/W': pt_blocks[i].attn.q_proj.weight.grad.T,
+                f'Block{i}/Attn/DenseQ/b': pt_blocks[i].attn.q_proj.bias.grad,
+                f'Block{i}/Attn/DenseK/W': pt_blocks[i].attn.k_proj.weight.grad.T,
+                f'Block{i}/Attn/DenseK/b': pt_blocks[i].attn.k_proj.bias.grad,
+                f'Block{i}/Attn/DenseV/W': pt_blocks[i].attn.v_proj.weight.grad.T,
+                f'Block{i}/Attn/DenseV/b': pt_blocks[i].attn.v_proj.bias.grad,
+                f'Block{i}/Attn/DenseO/W': pt_blocks[i].attn.output_proj.weight.grad.T,
+                f'Block{i}/Attn/DenseO/b': pt_blocks[i].attn.output_proj.bias.grad,
+                f'Block{i}/LNAttn/scale': pt_blocks[i].ln_attn.weight.grad,
+                f'Block{i}/LNAttn/shift': pt_blocks[i].ln_attn.bias.grad,
+            })
+        reference_gradients = {k: v.detach().numpy() for k, v in reference_gradients.items()}
+
+        activations, caches = [x.detach().numpy()], []
+        for block in my_blocks:
+            activation, cache = block.forward(activations[-1])
+            activations.append(activation)
+            caches.append(cache)
+        
+        loss_fn = losses.CrossEntropy(dim=-1)
+        output = optimizers.loss_and_grads(
+            x.detach().numpy(), y_hat.detach().numpy(), loss_fn, my_blocks
+        )
+        
+        for k, v in reference_gradients.items():
+            np_test.assert_allclose(v, output.gradients[k], atol=1e-5, rtol=1e-5)
+
+
+if __name__ == '__main__':
+    unittest.main()
+ 
