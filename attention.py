6a56e6e867ddfe3a7cf7aa847840338039f683da
Cole Gulino <colegulino@macbook-pro.lan>
2024-04-28 10:44:05 -0700

    Adds attention layers.

---
+++ attention.py
@@ -0,0 +1,122 @@
+"""Calculates multihead attention for an input sequence."""
+import math
+
+import numpy as np
+
+import activations
+import dense
+import layer
+
+
+def get_causal_mask(x: np.ndarray) -> np.ndarray:
+    """Gets causal mask for a sequence."""
+    B, T, _ = x.shape
+    # Shape: [T, T].
+    mask = np.triu(np.ones((T, T))) - np.eye(T)
+    # Shape: [B, T, T].
+    return mask[None].repeat(B, axis=0).astype(np.bool_)
+
+
+class MaskedSelfAttention(layer.Layer):
+
+    def __init__(self, embed_dim: int, use_bias: bool = True, name='MSA') -> None:
+        super().__init__(name)
+        self.embed_dim = embed_dim
+
+        self.q_proj = dense.Dense(embed_dim, embed_dim, name=f'{name}/DenseQ', use_bias=use_bias)
+        self.k_proj = dense.Dense(embed_dim, embed_dim, name=f'{name}/DenseK', use_bias=use_bias)
+        self.v_proj = dense.Dense(embed_dim, embed_dim, name=f'{name}/DenseV', use_bias=use_bias)
+        self.output_proj = dense.Dense(embed_dim, embed_dim, name=f'{name}/DenseO')
+        self.softmax = activations.Softmax(dim=-1)
+
+    def forward(
+            self, x: np.ndarray, mask: np.ndarray | None = None
+    ) -> tuple[np.ndarray, layer.Cache]:
+        """Runs forward pass of MH Self-Attention.
+        
+        Q = Wq * x; K = Wk * x; V = Wv * x
+        scores = softmax(Q @ K / scale)
+        O = Wo * scores @ K
+
+        Args:
+            x: Input to the module of shape [B, T, C].
+            mask: Attention mask of shape [B, T, T] of dtype bool.
+        
+        Returns:
+            Output of the attention module of shape: [B, T, C].
+        """
+        B, T, C = x.shape
+        # Shape: [B, T, C].
+        q = self.q_proj(x)
+        k = self.k_proj(x)
+        v = self.v_proj(x)
+
+        # Shape: [B, T, T].
+        scale = 1. / math.sqrt(k.shape[-1])
+        attn = q @ k.transpose([0, 2, 1]) * scale
+        if mask is not None:
+            # Shape: [B, T, T].
+            attn = np.where(mask, -float("inf"), attn)
+        scores = self.softmax(attn)
+
+        # Shape: [B, T, C // n_heads].
+        output = scores @ v
+        # Shape: [B, T, C].
+        y = self.output_proj(output)
+        cache = {
+            'q': q, 'k': k, 'v': v, 'attn': attn, 'scores': scores,
+            'output': output, 'mask': mask
+        }
+        return y, cache
+
+    def backward(
+            self,
+            x: np.ndarray,
+            cache: layer.Cache,
+            backwards_gradient: np.ndarray,
+            gradients: dict[str, np.ndarray]
+    ) -> np.ndarray:
+        """Backwards pass of the multi-headed attention function.
+
+        Args:
+            x: Input of the network of shape [B, T, C].
+            cache: Cache from the forward function to pass into the backward pass.
+            backwards_gradient: Gradient from the previous layer that represents
+                dout[prev_layer] / dout[this_layer] of shape [B, T, C].
+            gradients: Mutable dictionary which will not be updated as there are
+                no parameters of this function.
+        
+        Returns:
+            Gradient to pass onto the previous layer of shape [B, T, C] and
+                calculating.
+        """
+        B, T, C = x.shape
+
+        # Find the gradient w.r.t. the output projection.
+        # Shape: [B, T, C].
+        dy_doutput = self.output_proj.backward(
+            cache['output'], {}, backwards_gradient, gradients
+        )
+
+        # Get the gradients w.r.t. V
+        # Shape: [B,, T, C].
+        # doutput / dv -> Shape: [B,, T, C].
+        dy_dv = cache['scores'] @ dy_doutput
+        v_backwards = self.v_proj.backward(x, {}, dy_dv, gradients)
+        # Shape: [B, T, C].
+
+        # Get the gradients w.r.t. attention.
+        # Shape: [B,, T, T].
+        dy_dscores = backwards_gradient @ cache['v'].transpose([0, 2, 1])
+        # Shape: [B, T, T].
+        dy_dattn = self.softmax.backward(cache['attn'], {}, dy_dscores, {})
+        mask = cache['mask']
+        if mask is not None:
+            dy_dattn = dy_dattn * mask.astype(np.float32)
+
+        # Get the gradients w.r.t. Q and K.
+        # Shape: [B,, T, C].
+        k_backwards = self.k_proj.backward(x, {}, dy_dattn @ cache['q'], gradients)
+        q_backwards = self.q_proj.backward(x, {}, dy_dattn @ cache['k'], gradients)
+    
+        return k_backwards + q_backwards + v_backwards
---
+++ attention_test.py
@@ -0,0 +1,156 @@
+"""Test for attention in Numpy NN library."""
+
+import math
+import unittest
+
+from numpy import testing as np_test
+import torch
+from torch import nn
+
+import attention
+import activations
+import loss_base
+import losses
+
+
+class MSATorch(torch.nn.Module):
+
+    def __init__(self, embed_dim: int) -> None:
+        super().__init__()
+        self.embed_dim = embed_dim
+
+        self.q_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+        self.k_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+        self.v_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+        self.output_proj = torch.nn.Linear(self.embed_dim, self.embed_dim)
+    
+    def forward(self, x: torch.Tensor, mask: torch.Tensor | None = None) -> torch.Tensor:
+        B, T, C = x.shape
+        q = self.q_proj(x)
+        k = self.k_proj(x)
+        v = self.v_proj(x)
+
+        scale = 1. / math.sqrt(k.shape[-1])
+        attn = q @ k.transpose(-2, -1) * scale
+        if mask is not None:
+            attn = attn.masked_fill(mask, -float("inf"))
+        scores = torch.nn.functional.softmax(attn, dim=-1)
+        output = scores @ v
+        return self.output_proj(output)
+
+
+class TestAttention(unittest.TestCase):
+
+    def test_forward(self):
+        attn0_0 = MSATorch(embed_dim=10)
+        attn0_1 = MSATorch(embed_dim=10)
+
+        update_parameters = {
+            'MSA/DenseQ/W': attn0_0.q_proj.weight.T,
+            'MSA/DenseQ/b': attn0_0.q_proj.bias,
+            'MSA/DenseK/W': attn0_0.k_proj.weight.T,
+            'MSA/DenseK/b': attn0_0.k_proj.bias,
+            'MSA/DenseV/W': attn0_0.v_proj.weight.T,
+            'MSA/DenseV/b': attn0_0.v_proj.bias,
+            'MSA/DenseO/W': attn0_0.output_proj.weight.T,
+            'MSA/DenseO/b': attn0_0.output_proj.bias,
+            'MSA1/DenseQ/W': attn0_1.q_proj.weight.T,
+            'MSA1/DenseQ/b': attn0_1.q_proj.bias,
+            'MSA1/DenseK/W': attn0_1.k_proj.weight.T,
+            'MSA1/DenseK/b': attn0_1.k_proj.bias,
+            'MSA1/DenseV/W': attn0_1.v_proj.weight.T,
+            'MSA1/DenseV/b': attn0_1.v_proj.bias,
+            'MSA1/DenseO/W': attn0_1.output_proj.weight.T,
+            'MSA1/DenseO/b': attn0_1.output_proj.bias
+        }
+        update_parameters = {k: v.detach().numpy() for k, v in update_parameters.items()}
+        attn1_0 = attention.MaskedSelfAttention(embed_dim=10)
+        attn1_0.update_parameters(update_parameters)
+        attn1_1 = attention.MaskedSelfAttention(embed_dim=10, name='MSA1')
+        attn1_1.update_parameters(update_parameters)
+
+        x = torch.ones((5, 20, 10))
+        mask = attention.get_causal_mask(x)
+
+        y_ref = attn0_1(
+            attn0_0(x, torch.from_numpy(mask)), torch.from_numpy(mask)
+        )
+        y = attn1_1(attn1_0(x.detach().numpy(), mask), mask)
+        np_test.assert_allclose(y, y_ref.detach().numpy(), atol=1e-6, rtol=1e-6)
+
+    def test_backward(self):
+        attn0_0 = MSATorch(embed_dim=10)
+        attn0_1 = MSATorch(embed_dim=10)
+
+        update_parameters = {
+            'MSA/DenseQ/W': attn0_0.q_proj.weight.T,
+            'MSA/DenseQ/b': attn0_0.q_proj.bias,
+            'MSA/DenseK/W': attn0_0.k_proj.weight.T,
+            'MSA/DenseK/b': attn0_0.k_proj.bias,
+            'MSA/DenseV/W': attn0_0.v_proj.weight.T,
+            'MSA/DenseV/b': attn0_0.v_proj.bias,
+            'MSA/DenseO/W': attn0_0.output_proj.weight.T,
+            'MSA/DenseO/b': attn0_0.output_proj.bias,
+            'MSA1/DenseQ/W': attn0_1.q_proj.weight.T,
+            'MSA1/DenseQ/b': attn0_1.q_proj.bias,
+            'MSA1/DenseK/W': attn0_1.k_proj.weight.T,
+            'MSA1/DenseK/b': attn0_1.k_proj.bias,
+            'MSA1/DenseV/W': attn0_1.v_proj.weight.T,
+            'MSA1/DenseV/b': attn0_1.v_proj.bias,
+            'MSA1/DenseO/W': attn0_1.output_proj.weight.T,
+            'MSA1/DenseO/b': attn0_1.output_proj.bias
+        }
+        update_parameters = {k: v.detach().numpy() for k, v in update_parameters.items()}
+        attn1_0 = attention.MaskedSelfAttention(embed_dim=10)
+        attn1_0.update_parameters(update_parameters)
+        attn1_1 = attention.MaskedSelfAttention(embed_dim=10, name='MSA1')
+        attn1_1.update_parameters(update_parameters)
+
+        x = torch.ones((5, 20, 10))
+        mask = attention.get_causal_mask(x)
+
+        y_ref = attn0_1(
+            attn0_0(x, torch.from_numpy(mask)), torch.from_numpy(mask)
+        )
+        y = attn1_1(attn1_0(x.detach().numpy(), mask), mask)
+
+        y_hat = torch.tensor([0., 1., 0., 0., 0., 0., 0., 0., 0., 0.])[None, None].repeat(5, 20, 1)
+        
+        loss = torch.nn.CrossEntropyLoss()(
+            y_ref.contiguous().view(-1, 10), y_hat.contiguous().view(-1, 10)
+        )
+        loss.backward()
+
+        reference_gradients = {
+            'MSA/DenseQ/W': attn0_0.q_proj.weight.grad.T,
+            'MSA/DenseQ/b': attn0_0.q_proj.bias.grad,
+            'MSA/DenseK/W': attn0_0.k_proj.weight.grad.T,
+            'MSA/DenseK/b': attn0_0.k_proj.bias.grad,
+            'MSA/DenseV/W': attn0_0.v_proj.weight.grad.T,
+            'MSA/DenseV/b': attn0_0.v_proj.bias.grad,
+            'MSA/DenseO/W': attn0_0.output_proj.weight.grad.T,
+            'MSA/DenseO/b': attn0_0.output_proj.bias.grad,
+            'MSA1/DenseQ/W': attn0_1.q_proj.weight.grad.T,
+            'MSA1/DenseQ/b': attn0_1.q_proj.bias.grad,
+            'MSA1/DenseK/W': attn0_1.k_proj.weight.grad.T,
+            'MSA1/DenseK/b': attn0_1.k_proj.bias.grad,
+            'MSA1/DenseV/W': attn0_1.v_proj.weight.grad.T,
+            'MSA1/DenseV/b': attn0_1.v_proj.bias.grad,
+            'MSA1/DenseO/W': attn0_1.output_proj.weight.grad.T,
+            'MSA1/DenseO/b': attn0_1.output_proj.bias.grad
+        }
+
+        loss_fn = losses.CrossEntropy(dim=-1)
+        attn1_0_out, attn1_0_cache = attn1_0.forward(x.detach().numpy(), mask)
+        _, attn1_1_cache = attn1_1.forward(attn1_0_out, mask)
+        gradients = {}
+        backward_grad = loss_fn.backward(y, y_hat.detach().numpy())
+        backward_grad = attn1_1.backward(attn1_0_out, attn1_1_cache, backward_grad, gradients)
+        backward_grad = attn1_0.backward(x.detach().numpy(), attn1_0_cache, backward_grad, gradients)
+
+        for k, v in gradients.items():
+            np_test.assert_allclose(v, reference_gradients[k], rtol=1e-5, atol=1e-5)
+
+
+if __name__ == '__main__':
+    unittest.main()
