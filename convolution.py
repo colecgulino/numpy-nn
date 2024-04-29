4d7edadcc33fa268e91e0c697eb895d2c85d6d7e
Cole Gulino <colegulino@macbook-pro.lan>
2024-04-28 19:39:25 -0700

    Add convolution layers.

---
+++ convolution.py
@@ -0,0 +1,225 @@
+"""Convolutional network layers."""
+
+import itertools
+
+import numpy as np
+
+import initializers
+import layer
+
+
+def pad_image(image: np.ndarray, padding: int, padding_value: float = 0.) -> np.ndarray:
+    """Pads an image equally on all sides."""
+    B, H, W, C = image.shape
+    row_padding = np.ones(shape=(B, padding, W, C)) * padding_value
+    column_padding = np.ones(shape=(B, H + 2 * padding, padding, C)) * padding_value
+    image = np.concatenate((row_padding, image, row_padding), axis=1)
+    image = np.concatenate((column_padding, image, column_padding), axis=2)
+    return image
+
+
+def convolve_2d(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """Convolution 2D function.
+    
+    Args:
+        image: Image input of shape [B, H, W, in_channels, 1].
+        kernel: Filter to convolve over image of shape [1, 1, K, K, in_channels, out_channels].
+    
+    Returns:
+        Output of the 2d convolution filter of shape
+            [B, new_H, new_W, in_channels, out_channels].
+    """
+    K = kernel.shape[2]
+    hs = range(image.shape[1] - K + 1)
+    ws = range(image.shape[2] - K + 1)
+    chunks = []
+    for h, w in itertools.product(hs, ws):
+        chunks.append(image[:, h:h + K, w:w + K])
+    image = np.stack(chunks, axis=1)
+    return image * kernel
+
+
+def pad_input_sequence(seq: np.ndarray, padding: int, value: float = 0.) -> np.ndarray:
+    """Pad input sequence."""
+    B, T, C = seq.shape
+    # Shape: [B, padding, C].
+    pad = np.ones((B, padding, C), dtype=seq.dtype) * value
+    # Shape: [B, T + 2 * padding, C].
+    return np.concatenate((pad, seq, pad), axis=1)
+
+
+def convolve_1d(seq: np.ndarray, kernel: np.ndarray, kernel_dim: int) -> np.ndarray:
+    """Functional for the convolution operator."""
+    B, T = seq.shape[:2]
+    K = kernel.shape[kernel_dim]
+    # Shape: [B, num_chunks, K, in_channels].
+    chunks = []
+    for chunk_start in range(0, T - K + 1, 1):
+        chunks.append(seq[:, chunk_start:chunk_start + K])
+    seq = np.stack(chunks, axis=1)
+    # Shape: [B, num_chunks, K, in_channels, out_channels].
+    output = seq * kernel
+    return output
+
+
+class Conv2d(layer.Layer):
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int,
+            padding: int = 1,
+            padding_value: float = 0.,
+            name: str = 'Conv2D'
+    ) -> None:
+        super().__init__(name=name)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.padding_value = padding_value
+        self.stride = 1
+        self.set_parameter(
+            'W',
+            initializers.xavier_uniform(
+                in_channels,
+                out_channels,
+                shape=(kernel_size, kernel_size, in_channels, out_channels)
+            )
+        )
+        self.set_parameter('b', np.zeros([self.out_channels]))
+    
+    def forward(self, x: np.ndarray) -> tuple[np.ndarray, layer.Cache]:
+        """Forward propogation of the network"""
+        b, h, w = x.shape[:3]
+        x = pad_image(x, self.padding, self.padding_value)
+        weight, bias = self.get_parameter('W'), self.get_parameter('b')
+        output = convolve_2d(x[..., None], np.expand_dims(weight, axis=[0, 1]))
+        output = output.sum(2).sum(2).sum(-2)
+        new_h = (h - self.kernel_size + 2 * self.padding) // self.stride + 1
+        new_w = (w - self.kernel_size + 2 * self.padding) // self.stride + 1
+        return np.reshape(output, [b, new_h, new_w, self.out_channels]) + bias, {}
+
+    def backward(
+        self,
+        x: np.ndarray,
+        cache: layer.Cache,
+        backwards_gradient: np.ndarray,
+        gradients: dict[str, np.ndarray]
+    ) -> np.ndarray:
+        """Backwards method of the layer.
+        
+        Args:
+            x: Input to the layer.
+            cache: Cache from the forward function to pass into the backward pass.
+            backwards_gradient: Gradient from the previous layer that represents
+                dout[prev_layer] / dout[this_layer].
+            gradients: Mutable dictionary which should be updated with the
+                gradients for all parameters in this layer.
+            
+        Returns:
+            Gradients of this layer representing dout / din of the same shape
+                as the input to be sent back to the next downstream layer.
+        """
+        """Backward pass for a 1D convolutional network."""
+        del cache
+        image = pad_image(x, self.padding, self.padding_value)
+
+        gradients[f'{self.name}/b'] = backwards_gradient.sum(0).sum(0).sum(0)
+
+        # Convolve dout with the padded input.
+        dw = convolve_2d(image[..., None], np.expand_dims(backwards_gradient, axis=[1, -2]))
+        dw = dw.sum(0).sum(1).sum(1)
+        weight = self.get_parameter('W')
+        gradients[f'{self.name}/W'] = dw.reshape(weight.shape)
+
+        # Convolve the rotated filter by 180deg.
+        rotated_filter = np.rot90(weight, k=2, axes=(0, 1))
+        padding_needed = (self.kernel_size + x.shape[1] - 1 - backwards_gradient.shape[1]) // 2
+        padded_dout = pad_image(backwards_gradient, padding=padding_needed)
+        din = convolve_2d(
+            np.expand_dims(padded_dout, axis=-2),
+            np.expand_dims(rotated_filter, axis=[0, 1])
+        )
+        din = din.sum(2).sum(2).sum(-1).reshape(x.shape)
+        return din
+
+
+class Conv1D(layer.Layer):
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int = 3,
+            padding: int = 1,
+            padding_value: float = 0.,
+            name: str = 'Conv1D'
+    ) -> None:
+        super().__init__(name=name)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.padding = padding
+        self.padding_value = padding_value
+        self.kernel_size = kernel_size
+        self.set_parameter(
+            'W',
+            initializers.xavier_uniform(
+                self.in_channels,
+                self.out_channels,
+                shape=(kernel_size, in_channels, out_channels)
+            )
+        )
+        self.set_parameter('b', np.zeros((out_channels)))
+
+    def forward(self, x: np.ndarray) -> tuple[np.ndarray, layer.Cache]:
+        """Forward pass for the Conv1D layer.
+        
+        Args:
+            x: Input to the module of shape [B, T, in_channels].
+        
+        Returns:
+            Output of the module of shape [B, new_T, out_channels].
+        """
+        weight, bias = self.get_parameter('W'), self.get_parameter('b')
+        # Pad the input to new shape of [B, T + 2 * padding, in_channels].
+        x = pad_input_sequence(x, padding=self.padding, value=self.padding_value)
+        # Run the convolution operator over the sequence and the kernel.
+        output = convolve_1d(x[..., None], np.expand_dims(weight, axis=[0, 1]), kernel_dim=2)
+        output = output.sum(axis=2).sum(axis=2)
+        return output + bias, {}
+
+    def backward(
+        self,
+        x: np.ndarray,
+        cache: layer.Cache,
+        backwards_gradient: np.ndarray,
+        gradients: dict[str, np.ndarray]
+    ) -> np.ndarray:
+        """Backward pass for a 1D convolutional network."""
+        del cache
+        # Pad the input to new shape of [B, T + 2 * padding, in_channels].
+        x = pad_input_sequence(x, padding=self.padding, value=self.padding_value)
+
+        # Calculate the gradients of the bias.
+        gradients[f'{self.name}/b'] = backwards_gradient.sum(0).sum(0)
+
+        # Calculate the weights as a convolution of the error signal over the
+        # input.
+        dW = convolve_1d(x[..., None], np.expand_dims(
+            backwards_gradient, axis=[1, -2]), kernel_dim=2
+        )
+        gradients[f'{self.name}/W'] = dW.sum(0).sum(1)
+
+        din = np.zeros_like(x)
+        padding_needed = (self.kernel_size + 1) // 2 - self.padding
+        dout_padded = pad_input_sequence(
+            backwards_gradient, padding=padding_needed, value=self.padding_value
+        )
+
+        flipped_weight = np.flip(self.get_parameter('W'), axis=0)
+
+        kernel = np.expand_dims(flipped_weight, axis=[0, 1])
+        din = convolve_1d(dout_padded[..., None, :], kernel, kernel_dim=2).sum(2).sum(-1)
+        return din
---
+++ convolution_test.py
@@ -0,0 +1,176 @@
+"""Test for convolution layers."""
+
+import unittest
+
+import numpy as np
+from numpy import testing as np_test
+import torch
+from torch import nn
+
+import convolution
+import loss_base
+import losses
+import optimizers
+
+
+class Conv1DTest(unittest.TestCase):
+    
+    def test_forward(self):
+        B, T = 2, 6
+        x = np.random.random(size=(B, T, 10))
+        conv1d_1 = nn.Conv1d(in_channels=10, out_channels=20, kernel_size=3, padding=1)
+        conv1d_2 = nn.Conv1d(in_channels=20, out_channels=5, kernel_size=3, padding=1)
+
+        myconv1d_1 = convolution.Conv1D(10, 20)
+        myconv1d_2 = convolution.Conv1D(20, 5, name='Conv1D1')
+        myconv1d_1.update_parameters({
+            'Conv1D/W': conv1d_1.weight.permute([2, 1, 0]).detach().numpy(),
+            'Conv1D/b': conv1d_1.bias.detach().numpy()
+        })
+        myconv1d_2.update_parameters({
+            'Conv1D1/W': conv1d_2.weight.permute([2, 1, 0]).detach().numpy(),
+            'Conv1D1/b': conv1d_2.bias.detach().numpy()
+        })
+
+        x_torch = torch.from_numpy(x).permute([0, 2, 1]).float()
+        out1 = conv1d_2(conv1d_1(x_torch))
+        out1 = out1.permute([0, 2, 1])
+        out2 = myconv1d_2(myconv1d_1(x))
+        np_test.assert_allclose(out1.detach().numpy(), out2, atol=1e-6, rtol=1e-6)
+
+    def test_backward(self):
+        B, T = 2, 6
+        x = np.random.random(size=(B, T, 10))
+        conv1d_1 = nn.Conv1d(in_channels=10, out_channels=20, kernel_size=3, padding=1)
+        conv1d_2 = nn.Conv1d(in_channels=20, out_channels=5, kernel_size=3, padding=1)
+        x_torch = torch.from_numpy(x).permute([0, 2, 1]).float()
+        out1 = conv1d_2(conv1d_1(x_torch))
+        out1 = out1.permute([0, 2, 1])
+
+        y = np.array([0., 0, 1., 0., 0.])[None, None].repeat(B, axis=0).repeat(T, axis=1)
+        loss = nn.CrossEntropyLoss(reduction='sum')(
+            out1.reshape([-1, out1.shape[-1]]),
+            torch.from_numpy(y.copy()).reshape([-1, out1.shape[-1]])
+        )
+        loss.backward()
+        reference_gradients = {
+            'Conv1D/W': conv1d_1.weight.grad.permute([2, 1, 0]),
+            'Conv1D/b': conv1d_1.bias.grad,
+            'Conv1D1/W': conv1d_2.weight.grad.permute([2, 1, 0]),
+            'Conv1D1/b': conv1d_2.bias.grad,
+        }
+
+        myconv1d_1 = convolution.Conv1D(10, 20)
+        myconv1d_2 = convolution.Conv1D(20, 5, name='Conv1D1')
+        myconv1d_1.update_parameters({
+            'Conv1D/W': conv1d_1.weight.permute([2, 1, 0]).detach().numpy(),
+            'Conv1D/b': conv1d_1.bias.detach().numpy()
+        })
+        myconv1d_2.update_parameters({
+            'Conv1D1/W': conv1d_2.weight.permute([2, 1, 0]).detach().numpy(),
+            'Conv1D1/b': conv1d_2.bias.detach().numpy()
+        })
+
+        output = optimizers.loss_and_grads(
+            x,
+            y,
+            loss_fn=losses.CrossEntropy(reduction='sum', dim=-1),
+            layers=[myconv1d_1, myconv1d_2]
+        )
+
+        for k, v in reference_gradients.items():
+            np_test.assert_allclose(
+                v.detach().numpy(), output.gradients[k], atol=1e-5, rtol=1e-5
+            )
+
+
+class Conv2DTest(unittest.TestCase):
+    
+    def test_forward(self):
+        other_conv = nn.Conv2d(
+            in_channels=10,
+            out_channels=20,
+            kernel_size=3,
+            padding=0
+        )
+        other_conv2 = nn.Conv2d(
+            in_channels=20,
+            out_channels=5,
+            kernel_size=3,
+            padding=0
+        )
+
+        B, H, W, C = 2, 5, 5, 10
+        image = np.random.random(size=(B, H, W, C))
+        pytorch_image = torch.from_numpy(image).permute([0, 3, 1, 2]).float()
+        out1 = other_conv2(other_conv(pytorch_image)).permute([0, 2, 3, 1])
+        myconv = convolution.Conv2d(10, 20, 3, padding=0)
+        myconv.update_parameters({
+            'Conv2D/W': other_conv.weight.permute([2, 3, 1, 0]).detach().numpy(),
+            'Conv2D/b': other_conv.bias.detach().numpy(),
+        })
+        myconv2 = convolution.Conv2d(20, 5, 3, padding=0, name='Conv2D1')
+        myconv2.update_parameters({
+            'Conv2D1/W': other_conv2.weight.permute([2, 3, 1, 0]).detach().numpy(),
+            'Conv2D1/b': other_conv2.bias.detach().numpy(),
+        })
+
+        out2 = myconv2(myconv(image))
+        np_test.assert_allclose(out1.detach().numpy(), out2, atol=1e-6, rtol=1e-6)
+
+    def test_backward(self):
+        other_conv = nn.Conv2d(
+            in_channels=10,
+            out_channels=20,
+            kernel_size=3,
+            padding=0
+        )
+        other_conv2 = nn.Conv2d(
+            in_channels=20,
+            out_channels=5,
+            kernel_size=3,
+            padding=0
+        )
+
+        B, H, W, C = 2, 5, 5, 10
+        image = np.random.random(size=(B, H, W, C))
+        pytorch_image = torch.from_numpy(image).permute([0, 3, 1, 2]).float()
+        out1 = other_conv2(other_conv(pytorch_image)).permute([0, 2, 3, 1])
+        y = np.array([0., 0, 1., 0., 0.])[None, None, None].repeat(
+            B, axis=0).repeat(out1.shape[1], axis=1).repeat(out1.shape[2], axis=2)
+        loss = nn.CrossEntropyLoss(reduction='sum')(
+            out1.reshape([-1, out1.shape[-1]]),
+            torch.from_numpy(y.copy()).reshape([-1, out1.shape[-1]])
+        )
+        loss.backward()
+        reference_gradients = {
+            'Conv2D/W': other_conv.weight.grad.permute([2, 3, 1, 0]).detach().numpy(),
+            'Conv2D/b': other_conv.bias.grad.detach().numpy(),
+            'Conv2D1/W': other_conv2.weight.grad.permute([2, 3, 1, 0]).detach().numpy(),
+            'Conv2D1/b': other_conv2.bias.grad.detach().numpy(),
+        }
+
+        myconv = convolution.Conv2d(10, 20, 3, padding=0)
+        myconv.update_parameters({
+            'Conv2D/W': other_conv.weight.permute([2, 3, 1, 0]).detach().numpy(),
+            'Conv2D/b': other_conv.bias.detach().numpy(),
+        })
+        myconv2 = convolution.Conv2d(20, 5, 3, padding=0, name='Conv2D1')
+        myconv2.update_parameters({
+            'Conv2D1/W': other_conv2.weight.permute([2, 3, 1, 0]).detach().numpy(),
+            'Conv2D1/b': other_conv2.bias.detach().numpy(),
+        })
+
+        output = optimizers.loss_and_grads(
+            image,
+            y,
+            loss_fn=losses.CrossEntropy(reduction='sum', dim=-1),
+            layers=[myconv, myconv2]
+        )
+
+        for k, v in reference_gradients.items():
+            np_test.assert_allclose(v, output.gradients[k], atol=1e-5, rtol=1e-5)
+
+
+if __name__ == '__main__':
+    unittest.main()
